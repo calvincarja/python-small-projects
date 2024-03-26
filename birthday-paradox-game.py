816fc22a8efabb89783e5bcf589d3e833dbce632
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-03-26 03:51:10 +0000

    completed dictionary code

--- birthday-paradox-game.py
+++ birthday-paradox-game.py
@@ -16,7 +16,7 @@ steps to demonstrate birthday paradox:
 '''
 
 def instructions():
-    return print("""
+    print("""
      Hello - want to learn a fun fact? walk into a room filled with people, and depending on the amount of people,
      there is a strong probability that mulitple people in that room share a birthday.
      I can demonstrate this to you with a series of trails.
@@ -27,6 +27,7 @@ def data ():
     month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     day = list(range(1, 32)) # use list function to pass range(1,32), avoding to need to type 1 - 31
     return month, day
+
 '''
 # lets try to use a random tool to select a month and day
 
@@ -35,9 +36,7 @@ random_day = random.choice(day)
 
 print(f"The randomly selected date is {random_month} {random_day}")
 print(type(random_month))
-'''
 
-'''
 # now lets try to display multiple random chocies of month and day
 sample_size = 5
 random_list = [(random.choice(month), random.choice(day)) for i in range(sample_size)] # [will contain the list] (is the tuples inside the list - seperated by a comma)
@@ -60,6 +59,7 @@ for i in random_list:
     # remove the paranthesis from each value
     # since my list contains tuples, i can use two for loop iterator to address them each
 '''
+
 def calculate_random(month, day): # lets calculate and display random_list, pass month, day rather than re-initilizing it inside the function
     sample_size = int(input('how many birthdays should I generate: '))
     random_list = [(random.choice(month), random.choice(day)) for i in range(sample_size)] # [will contain the list] (is the tuples inside the list - seperated by a comma)
@@ -78,10 +78,7 @@ however I want the output to be in one line,
 to so, i need to store the output in a list, in order to use the .join() function
 the problem ask to identify if a birthday is shared by more than 1 person, 
 it does not ask me for the specific birthday(s), thus, finding the MODE is sufficient
-'''
 
-
-'''
 my function below has a few errors:
 random_list = calculate_random() is generating a new random list, i do not want that, i want to keep the random list i generated in my function above
 
@@ -105,15 +102,24 @@ def find_shared_bday(random_list):
 
 def find_shared_bday(random_list): # passing random_list will keep the list generated in the above
     birth_month_day = {} # will store the key:value pairs
-    for month, day in random_list: # storing tuple in dictionary
-        birth_month_day[month, day] = birth_month_day.get((month, day), 0) + 1
-    return birth_month_day
+    for month, day in random_list: 
+        birth_month_day[month, day] = birth_month_day.get((month, day), 0) + 1 # stores tuples in dictionary
+    
+    shared_birthday = False # created varible to avoid having return statements inside my for loop
+    for key, value in birth_month_day.items(): # key == (month, day) tuple, value == the count for each tuple, .items() is view-object of the key:value pair within the dictionary
+        if value > 1:
+            print(f"the birthday {key[0]} {key[1]} were shared by multiple people")
+            shared_birthday = True # removed the return from the for loop - it immeditly stops the loop after first iterator
+    if not shared_birthday:
+        print('no birthdays were shared')
+    # no return statment needed as we're not storing any result of this function in a varible, or expresssion. - as of yet
+    
 
 def initiate():
     instructions() 
     month, day = data() # call data function to unpack its return values
     random_list = calculate_random(month, day)
-    print(find_shared_bday(random_list))
+    find_shared_bday(random_list)
 
 '''
 now i need my code to run x amount of times
