843dce2a3407fda4a85592180b2626fcc44920c2
Calvin Pineda <151392191+calvincarja@users.noreply.github.com>
2024-03-26 02:01:17 +0000

    began creating functions for my code

--- birthday-paradox-game.py
+++ birthday-paradox-game.py
@@ -15,16 +15,18 @@ steps to demonstrate birthday paradox:
     find the month + day combination repeated the most time, output this result to the user
 '''
 
-
-print("""
+def instructions():
+    return print("""
      Hello - want to learn a fun fact? walk into a room filled with people, and depending on the amount of people,
      there is a strong probability that mulitple people in that room share a birthday.
      I can demonstrate this to you with a series of trails.
      Lets begin!
       """)
-month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-day = list(range(1, 32)) # use list function to pass range(1,32), avoding to need to type 1 - 31
 
+def data ():
+    month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    day = list(range(1, 32)) # use list function to pass range(1,32), avoding to need to type 1 - 31
+    return month, day
 '''
 # lets try to use a random tool to select a month and day
 
@@ -58,11 +60,14 @@ for i in random_list:
     # remove the paranthesis from each value
     # since my list contains tuples, i can use two for loop iterator to address them each
 '''
-
-sample_size = int(input('how many birthdays should I generate: '))
-random_list = [(random.choice(month), random.choice(day)) for i in range(sample_size)] # [will contain the list] (is the tuples inside the list - seperated by a comma)
-print(f"below are the {sample_size} birthdays:")
-
+def calculate_random(): # lets calculate and display random_list
+    month, day = data()
+    sample_size = int(input('how many birthdays should I generate: '))
+    random_list = [(random.choice(month), random.choice(day)) for i in range(sample_size)] # [will contain the list] (is the tuples inside the list - seperated by a comma)
+    print(f"below are the {sample_size} birthdays:")
+    results = ', '.join([f"{month} {day}" for month, day in random_list]) # in order to print in one line
+    print(results)
+    return random_list
 '''
 for month, day in random_list:
     print(f"{month} {day},")
@@ -70,23 +75,26 @@ for month, day in random_list:
 the above for loop address all three concerns,
 however I want the output to be in one line, 
 to so, i need to store the output in a list, in order to use the .join() function
-    
-'''
-results = ', '.join([f"{month} {day}" for month, day in random_list]) # in order to print in one line
-print(results)
-
-'''
 the problem ask to identify if a birthday is shared by more than 1 person, 
 it does not ask me for the specific birthday(s), thus, finding the MODE is sufficient
 '''
 
 # sucess = print(mode) or print('The list contains multiple modes')
-try:
-    mode = statistics.mode(random_list)
-    print(f"mutliple people share a birthday on: {mode[0]} {mode[1]}")
-except statistics.StatisticsError as e:
-    if 'no unique mode' in str(e): # no unqiue mode signifies more than one mode
-        print('multiple people share multiple birthdays')
-    else:
-        raise
+def calculate_mode(random_list):
+    random_list = calculate_random()
+    try:
+        mode = statistics.mode(random_list)
+        print(f"mutliple people share a birthday on: {mode[0]} {mode[1]}")
+    except statistics.StatisticsError as e:
+        if 'no unique mode' in str(e): # no unqiue mode signifies more than one mode
+            print('multiple people share multiple birthdays')
+        else:
+            raise
+
+'''
+now i need my code to run x amount of times
+each time it runs, it needs to keep track if a mode was found (single or multiple mode)
+once the simulation is over, i need to output the amount of times a mode was found
+i then divide by the x amount of times it ran, and generate a average percentage 
+'''
 
